1ce72b604e2e5d37f7bfc75838750a9bd5e8a054
EugeneKelembet <79920734+EugeneKelembet@users.noreply.github.com>
2022-11-03 00:04:01 +0200

    Update DetailedDataSchema.sql

--- 2-SoftwareDesign/2.4-DetailedDataSchema/DetailedDataSchema.sql
+++ 2-SoftwareDesign/2.4-DetailedDataSchema/DetailedDataSchema.sql
@@ -2,13 +2,56 @@ drop table user CASCADE;
 drop table parameters CASCADE;
 
 CREATE TABLE user(
-	id_user INT PRIMARY KEY , 
-	questions VARCHAR
+	id_user INT , 
+	questions VARCHAR REFERENCES office_worker(id_question),
+	PRIMARY KEY (id_user, questions)
 );
 
 CREATE TABLE parameters(
 	e-mail VARCHAR,
 	password VARCHAR,
 	id_user INT REFERENCES user(id_user),
-  PRIMARY KEY (id_user)
+        PRIMARY KEY (id_user)
+);
+
+CREATE TABLE autentification(
+	parameters VARCHAR,
+	id_user INT REFERENCES parameters(id_user),
+	id_autentification INT PRIMARY KEY
+);
+
+CREATE TABLE software(
+	indicators INT,
+	id_session INT REFERENCES autentification(id_autentification)
+);
+
+CREATE TABLE sculpture(
+	name VARCHAR,
+	description VARCHAR,
+	type VARCHAR,
+	cost INT,
+	author VARCHAR REFERENCES user(id_user),
+	material VARCHAR,
+	id_sculpture INT PRIMARY KEY
+);
+
+CREATE TABLE indicators(
+	temperature INT,
+	hunidity INT ,
+	id_sculpture REFERENCES sculpture(id_sculpture)
+);
+
+CREATE TABLE office_worker(
+	id_question INT PRIMARY KEY,
+	id_answer INT
+);
+
+CREATE TABLE questions(
+	id_question INT REFERENCES office_worker(id_question),
+	id_answer INT REFERENCES search_software(id_answer)
+);
+
+CREATE TABLE search_software(
+	id_question INT,
+	id_answer INT PRIMARY KEY
 );
