6b632b85eeac312af5f7b258e4d84a240fbb398a
EugeneKelembet <79920734+EugeneKelembet@users.noreply.github.com>
2022-11-02 22:21:27 +0200

    Update DetailedDataSchema.sql

--- 2-SoftwareDesign/2.4-DetailedDataSchema/DetailedDataSchema.sql
+++ 2-SoftwareDesign/2.4-DetailedDataSchema/DetailedDataSchema.sql
@@ -1,1 +1,14 @@
+drop table user CASCADE;
+drop table parameters CASCADE;
 
+CREATE TABLE user(
+	id_user INT PRIMARY KEY , 
+	questions VARCHAR
+);
+
+CREATE TABLE parameters(
+	e-mail VARCHAR,
+	password VARCHAR,
+	id_user INT REFERENCES user(id_user),
+  PRIMARY KEY (id_user)
+);
