a93e67d10a9d222a6f16e1d2d0829972b062f8be
EugeneKelembet <79920734+EugeneKelembet@users.noreply.github.com>
2022-11-18 22:45:42 +0200

    Update 5-Program-Methods-Using-Example.java

--- 3-SoftwareConstruction/5-Program-Methods-Using-Example.java
+++ 3-SoftwareConstruction/5-Program-Methods-Using-Example.java
@@ -1,1 +1,45 @@
+package com.nuop.tscp.model.user;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+public class UserController {
+
+  private final UserService userService;
+
+  @Autowired
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @GetMapping("/users")
+  public String getAllUsers(Model model) {
+
+    List<User> users = userService.getAllUsers();
+    model.addAttribute("users", users);
+    System.out.println("USERS = " + users);
+
+    return "usersPage";
+  }
+
+  @GetMapping("/users/add")
+  public String getAddUserPage() {
+
+    return "addUserPage";
+  }
+
+  @PostMapping("/users")
+  public String addUser(@RequestBody UserForm userForm) {
+
+    User user = userService.createUser(userForm);
+    userService.saveUser(user);
+
+    return "redirect:/users";
+  }
+}
